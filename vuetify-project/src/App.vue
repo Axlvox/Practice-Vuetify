12f6d2d2970fe431e0d58c486dabc5ab9a38d81a
Axlvox <leandro.jlds@gmail.com>
2024-06-04 10:37:38 -0300

    Create AppBar

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-app>
+  <v-app theme="light">
+    <v-app-bar flat color="#5F4BB6" class="border-b" title="App">
+    </v-app-bar>
+    <v-navigation-drawer permanent>
+    </v-navigation-drawer>
     <v-main>
       <HelloWorld />
     </v-main>
