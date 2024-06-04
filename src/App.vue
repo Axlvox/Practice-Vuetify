642419a1561c412830c717a45bebbd642a4b6c9b
Axlvox <leandro.jlds@gmail.com>
2024-06-04 11:11:50 -0300

    Add box tests

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,16 @@
     <v-navigation-drawer permanent>
     </v-navigation-drawer>
     <v-main>
-      <HelloWorld />
+   <v-container>
+    <div class="d-flex justify-space-between">
+     <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 1
+    </div>
+    <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 2
+    </div>
+    <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 3
+    </div>
+    </div>
+   </v-container>
     </v-main>
 
     <AppFooter />
