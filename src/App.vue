02682eefb71b85a9ab10ccdf67a32d651a74ed64
Axlvox <leandro.jlds@gmail.com>
2024-06-04 11:37:29 -0300

    Fix box

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <v-app theme="light">
     <v-app-bar flat color="#5F4BB6" class="border-b" title="App">
     </v-app-bar>
-    <v-navigation-drawer permanent>
+    <v-navigation-drawer>
     </v-navigation-drawer>
     <v-main>
    <v-container>
-    <div class="d-flex justify-space-between">
-     <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 1
-    </div>
-    <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 2
-    </div>
-    <div class="w-100 pa-4 border rounded-lg -elevation-2 text-center">Box 3
-    </div>
+    <div class="d-flex justify-space-around">
+      <div class="pa-4 border rounded-lg elevation-2 text-center" style="width: 100px">Box 1</div>
+      <div class="pa-4 border rounded-lg elevation-2 text-center" style="width: 100px">Box 2</div>
+      <div class="pa-4 border rounded-lg elevation-2 text-center" style="width: 100px">Box 3</div>
     </div>
    </v-container>
     </v-main>
