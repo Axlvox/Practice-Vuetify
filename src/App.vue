6a329596ce7d803535e29211b2aac85d39bd7bf1
Axlvox <leandro.jlds@gmail.com>
2024-06-05 09:06:42 -0300

    Add bell

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
     <v-app-bar flat color="#5F4BB6" class="border-b">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-app-title>Menu</v-app-title>
-      <v-avatar>
+      <v-spacer></v-spacer>
+        <v-badge class="mr-5" :content="6">
+          <v-icon icon="mdi-bell-outline"></v-icon>
+        </v-badge>
+      <v-avatar class="mr-5">
           <v-img 
-          class="" 
+          class=""
           cover 
-          src="https://www.creativefabrica.com/wp-content/uploads/2021/03/10/Women-avatar-icon-Graphics-9452550-1-580x386.jpg"></v-img>
+          src="https://cdn3.iconfinder.com/data/icons/avatar-set/512/Avatar11-512.png"></v-img>
         </v-avatar>
     </v-app-bar>
     <v-navigation-drawer>
